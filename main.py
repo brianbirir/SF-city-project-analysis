925c650e2cbefd7b556c212d3e12cf6aa0df114b
brianbirir <brianbirir@gmail.com>
2019-05-16 13:16:51 +0300

    feature(tranform): Preprocess description column data

--- main.py
+++ main.py
@@ -12,3 +12,46 @@ def dataset_reader():
     # convert Excel file to data frame
     records = pd.read_excel(FILE_SOURCE)
     return records
+
+
+def get_description_column():
+    """Extracts the description column from the data source
+
+    Args:
+        file_source: An Excel file type
+
+    Returns:
+        dataFrame: The pandas data structure with the description column
+    """
+    df = dataset_reader()
+    return df[['description']]
+
+
+def remove_punctuation(text):
+    """Removes punctuation from the description column text
+
+    Returns:
+        dataFrame: Description column data without punctuation
+    """
+    symbols = "[^\w\s]"
+    return text.str.replace(symbols, '')
+
+
+def to_lowercase(text):
+    """Converts description column text to lower case
+
+    Returns:
+        dataFrame: Description column data text in lower case
+    """
+    return text.str.lower()
+
+
+def preprocess(data):
+    """Preprocess of text data before analysis
+
+    Returns:
+        string: Preprocessed text
+    """
+    data = remove_punctuation(data)
+    preprocessed_data = to_lowercase(data)
+    return preprocessed_data
